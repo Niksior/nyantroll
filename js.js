030e5cc3f5c8da12d8dde3c1741c8957906c781b
Niksior <heroes.VS.misio@gmail.com>
2015-11-06 21:33:48 +0100

    czcionka

--- js.js
+++ js.js
@@ -1,12 +1,10 @@
 WebFontConfig = {
-  google: { families: [ 'Francois+One::latin,latin-ext' ] }
-};
-(function() {
-  var wf = document.createElement('script');
-  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-  wf.type = 'text/javascript';
-  wf.async = 'true';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(wf, s);
+    google: { families: [ 'Press+Start+2P::latin-ext', 'Play::latin-ext' ] } };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
 })();
