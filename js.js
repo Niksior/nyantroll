e1d5b98d1db5ef024950b55c392d7bcbf4849c2e
Niksior <heroes.VS.misio@gmail.com>
2015-11-08 19:59:18 +0100

    dodatki

--- js.js
+++ js.js
@@ -1,3 +1,7 @@
+if( navigator.userAgent.toLowerCase().indexOf('firefox') > -1 ){
+	alert("Zmien przegladarke na:\nChrome 47-\nOpera12+\nEdge\nIE11+\nGdyz strona moze zle dzialac");
+}
+
 WebFontConfig = {
     google: { families: [ 'Press+Start+2P::latin-ext', 'Play::latin-ext' ] } };
   (function() {
@@ -14,30 +18,32 @@ function tgMenu(menu){
 	var menu = document.getElementById(menu);
 	if(menu.dataset.opened == "no"){
 	    menu.style.transform = "perspective(0px) rotateY(0deg)";
+	    menu.style.transform = "perspective(0px) rotateY(0deg)";
 		menu.dataset.opened = "yes";
 		return false;
 	} else {
+		menu.style.transform = "perspective(0px) rotateY(90deg)";
 		menu.style.transform = "perspective(0px) rotateY(90deg)";
 		menu.dataset.opened = "no";
 	}
 }
 
-	var a=1;
-	
+var a=1;
+
 function bgclr(bg, tb, mn){
 	var bg = document.getElementById(bg);
 	var tb = document.getElementById(tb);
 	var mn = document.getElementById(mn);
 	if(a==1){
-		bg.style.background = "#FF7043"
-		tb.style.background = "#BF360C"
-		mn.style.background = "#FFCCBC"
+		bg.style.background = "#EDE7F6";
+		tb.style.background = "#673AB7";
+		mn.style.background = "#9575CD";
 		a=0;
 	}
 	else{
-		bg.style.background = "#FBE9E7"
-		tb.style.background = "#FF5722"
-		mn.style.background = "#FF7043"
-		a=1
+		bg.style.background = "#FBE9E7";
+		tb.style.background = "#FF5722";
+		mn.style.background = "#FF7043";
+		a=1;
 	}
 }
